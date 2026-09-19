2fdebb34f350955567553a22a18abcdda35986fd
Arpita Giri <arpitagiri2004@gmail.com>
2026-09-20 00:27:31 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -62,8 +62,8 @@ app.get("/api/voices", (req, res) => {
     });
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
